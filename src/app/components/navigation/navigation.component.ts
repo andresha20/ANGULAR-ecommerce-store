9dbac2269b63bf567e118afd8994ffb8f10de2bb
andresha20 <andreshenao.2021@gmail.com>
2023-03-28 23:02:25 -0500

    28/03/2023 - 11:02

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-navigation',
@@ -6,8 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
+  constructor (
+    public appService: AppService
+  ) {}
   navigationItems = [
     { url: '', name: 'Home' },
     { url: 'shop', name: 'Shop' },
   ];
+
+  openCart() {
+    this.appService.setIsCartModalOpen(!this.appService.isCartModalOpen);
+  }
 }
